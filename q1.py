13b04d029e53b4e0e4f6f236fbb51db571c7a101
akil pc <aqil.farouq99@gmail.com>
2021-12-08 23:42:40 +0800

    q1 and q2 code

---
+++ q1.py
@@ -0,0 +1,272 @@
+import threading
+import time
+import random
+import math
+import numpy as np
+
+simTime = 0
+simEnd = 5
+
+food_list = ['Mee goreng mamak', 'Apam balik', 'Nasi kerabu','Ayam percik', 'Nasi lemak', 
+             'Roti john', 'Rendang', 'Kuih', 'Nasi kandar', 'Laksa', 
+             'Popia basah', 'Bubur', 'Roti jala', 'Cendawan goreng', 'Sambal udang']
+
+class Restaurant(threading.Thread):
+    IS_OPEN = 0
+    IS_FULL = 1
+    def __init__(self,foods,capacity,grid,loc):
+        super().__init__()
+        self.foods = foods
+        self.capacity = capacity
+        self.waitingTime = [0,threading.Lock()]
+        self.seats = [None for i in range(math.ceil(capacity/2))]
+        self.seatsLock = threading.Lock()
+        self.status = Restaurant.IS_OPEN
+        self.staff = 0
+        self.totalStuff = 0
+        self.sales = 0
+        self.grid = grid
+        self.loc = loc
+        self.grid.arr[loc[0]][loc[1]] = []
+        self.newCustomer = None
+    
+    def run(self):
+        global simTime, simEnd
+        while simTime < simEnd:
+            self.seatsLock.acquire()
+            if all(seat is not None for seat in self.seats): # if there's no open seat
+                self.status = Restaurant.IS_FULL
+            else: # got seat
+                self.status = Restaurant.IS_OPEN
+                x = self.loc[0]
+                y = self.loc[1]
+                self.grid.locks[x][y].acquire()
+                if isinstance(self.grid.arr[x][y][0],Customer):
+                    self.newCustomer = self.grid.getFromQueue(x,y)
+                    self.grid.removeFromQueue(x,y)
+                    while True:
+                        newSeat = random.randint(0,math.ceil(self.capacity/2)-1)
+                        if not isinstance(newSeat,Customer):
+                            self.seats[newSeat] = self.newCustomer
+                            self.newCustomer.status = self.newCustomer.eatWay
+                            self.newCustomer = None
+                self.grid.locks[x][y].release()
+            self.seatsLock.release()
+
+            for i in self.seats:
+                if isinstance(i,Customer):
+                    if i.status is Customer.IS_DONE:
+                        index = self.seats.index(i)
+                        self.seats[index] = None
+                        self.sales += 1
+            if self.sales%5 == 0:
+                self.staff += 1
+            time.sleep(10)
+
+class Staff(threading.Thread):
+    def __init__(self,loc,grid,restaurant):
+        super().__init__()
+        self.customers = []
+        self.loc = loc
+        self.grid = grid
+        self.restaurant = restaurant
+    
+    def run(self):
+        global simTime, simEnd
+        while simTime < simEnd:
+            people = 0
+            for i in range(3):
+                for j in range(3):
+                    nx = self.loc[0] + i - 1
+                    ny = self.loc[1] + j - 1
+
+                    if nx < 0 or ny < 0:
+                        continue
+                    if nx >= self.grid.width or ny >= self.grid.height:
+                        continue
+                    if nx == self.loc[0] and ny == self.loc[1]:
+                        continue
+                    self.grid.locks[nx][ny].acquire()
+                    potentialCustomer = self.grid.get(nx,ny)
+                    if potentialCustomer != None:
+                        if any(favFood is [food for food in self.restaurant.foods] for favFood in potentialCustomer.favFood) and potentialCustomer.status == Customer.IS_EXIST:
+                            potentialCustomer.haveFavFood = True
+                            potentialCustomer.status = Customer.IS_THINKING
+                            potentialCustomer.staff = self
+                            time.sleep(2)
+                            self.grid.locks[nx][ny].release()                            
+                            people += 1
+                    
+            while people == 0:
+                Break = False
+                x = random.randint(0,self.grid.width+1)
+                y = random.randint(0,self.grid.height+1)
+
+                self.grid.locks[x][y].acquire()
+                if isinstance(self.grid.getFromQueue(self.grid.arr[x][y]),Staff) or isinstance(self.grid.getFromQueue(self.grid.arr[x][y]),Customer) or isinstance(self.grid.getFromQueue(self.grid.arr[x][y]), Restaurant):
+                    self.grid.locks[x][y].release()
+                    continue
+                else:
+                    self.grid.arr[x][y] = self
+                    self.loc =[x,y]
+                    self.grid.locks[x][y].release()
+
+                for i in range(3):
+                    for j in range(3):
+                        nx = x + i - 1
+                        ny = y + j - 1
+
+                        if nx < 0 or ny < 0:
+                            continue
+                        if nx >= self.grid.width or ny >= self.grid.height:
+                            continue
+                        if nx == self.loc[0] and ny == self.loc[1]:
+                            continue
+                        self.grid.locks[nx][ny].acquire()
+                        potentialCustomer = self.grid.get(nx,ny)
+                        if potentialCustomer != None:
+                            people += 1
+                            Break = True
+                        self.grid.locks[nx][ny].release()
+                        if Break: break
+                    if Break: break
+        time.sleep(1)
+
+class Customer(threading.Thread):
+    IS_EXIST = 0
+    IS_WAITING_IN_QUEUE = 1
+    IS_EATING_ENJOYING = 2
+    IS_EATING = 3
+    IS_DONE = 4
+    HAVE_CHOSEN = 5
+    IS_THINKING = 6
+
+    def __init__(self,favFood,loc,grid):
+        super().__init__()
+        self.favFood = favFood
+        self.waitingTime = 0
+        self.loc = loc
+        self.status = Customer.IS_EXIST
+        self.grid = grid
+        self.seats = None
+        self.haveFavFood = None
+        self.randomRestaurant = False
+        self.eatWay = random.choice([Customer.IS_EATING,Customer.IS_EATING_ENJOYING])
+        self.staff = None
+        self.acceptProb = 0
+    
+    def run(self):
+        global simTime, simEnd
+        while simTime < simEnd:
+            if self.status == Customer.IS_EATING or Customer.IS_EATING_ENJOYING:
+                time.sleep(random.randint(1,6))
+                self.status = Customer.IS_DONE
+            
+            if self.haveFavFood:
+                self.grid.locks[self.loc[0]][self.loc[1]].acquire()
+                self.grid.addToQueue(self, self.loc[0],self.loc[1])
+                self.grid.remove(self.loc[0],self.loc[1])
+                self.grid.locks[self.loc[0]][self.loc[1]].remove()
+                self.loc = None
+                self.status = Customer.IS_WAITING_IN_QUEUE
+            
+            if self.status == Customer.IS_WAITING_IN_QUEUE:
+                self.waitingTime += 1
+            
+            if self.status == Customer.IS_THINKING:
+                time.sleep(random.randint(0,2))
+                queueNumber = len(self.grid.arr[self.staff.restaurant.loc[0]][self.staff.restaurant.loc[1]])
+
+                self.acceptProb = queueNumber/100
+                if self.haveFavFood:
+                    self.acceptProb +=0.6
+                dec = np.random.choice([Customer.IS_THINKING,Customer.HAVE_CHOSEN],1,[self.acceptProb, 1 - self.acceptProb])
+                dec = dec[0]
+
+                if dec == Customer.IS_THINKING:
+                    self.status = Customer.IS_EXIST
+                    self.haveFavFood = False
+                    lookLoc = []
+                    while True:
+                        for i in range(3):
+                            for j in range(3):
+                                nx = self.loc[0] + i - 1
+                                ny = self.loc[1] + j - 1
+                                lookLoc.append([nx,ny])
+
+                        newLoc = random.choice(lookLoc)
+
+                        self.grid.locks[nx][nx].acquire()
+                        if isinstance(self.grid.arr[nx][nx],Staff) or isinstance(self.grid.arr[nx][nx],Customer):
+                            self.grid.locks[nx][nx].release()
+                            continue
+                        else:
+                            self.grid.arr[nx][nx] = self
+                            self.loc =[nx,nx]
+                            self.grid.locks[nx][nx].release()
+                            break
+                else:
+                    self.status = Customer.HAVE_CHOSEN
+
+            time.sleep(1)
+
+class Grid:
+    def __init__(self, dim):
+        self.width = dim
+        self.height = dim
+        self.arr = [[ None for i in range(dim)] for j in range(dim)]
+        self.locks = [[None for i in range(dim)] for j in range(dim)]
+        
+        for j in range(dim):
+            for i in range(dim):
+                self.locks[j][i] = threading.Lock()
+
+    def addToQueue(self, entity, x, y):
+        self.arr[x][y].append(entity)
+        
+    def getFromQueue(self, x, y):
+        return self.arr[y][x][0]
+        
+    def removeFromQueue(self, x, y):
+        self.arr[y][x].remove(self.arr[y][x][0])
+
+    def add(self, entity, x, y):
+        self.arr[x][y] = entity
+        
+    def get(self, x, y):
+        return self.arr[x][y]
+        
+    def remove(self, x, y):
+        self.arr[x][y] = None
+
+bazaar = Grid(50)
+restaurants = []
+for i in range(3):
+    while True:
+        loc = [round(random.randint()*50),round(random.randint()*50)]
+        if not(isinstance(bazaar.getFromQueue(loc[0],loc[1]), Restaurant)):
+            break
+    restaurant = Restaurant(random.choices(food_list,k=3),random.randint(10,21),bazaar,loc)
+    restaurants.append(restaurant)
+    restaurant.start()
+
+while simTime < simEnd:
+    for i in range(3):
+        while True:
+            loc = [round(random.randint()*50),round(random.randint()*50)]
+            if not(isinstance(bazaar.getFromQueue(loc[0],loc[1]), Restaurant) or isinstance(bazaar.getFromQueue(loc[0],loc[1]), Customer) or isinstance(bazaar.getFromQueue(loc[0],loc[1]), Staff)):
+                break
+        newStaff = restaurants[i].staff - restaurants[i].totalStuff
+        for i in range(newStaff):
+            staff = Staff(loc,bazaar,restaurants[i])
+        restaurants[i].totalStuff = restaurants[i].staff + newStaff 
+    
+    newCustomerAmount = random.randint(0,5)
+    for i in range(newCustomerAmount):
+        while True:
+            loc = [round(random.randint()*50),round(random.randint()*50)]
+            if not(isinstance(bazaar.getFromQueue(loc[0],loc[1]), Restaurant) or isinstance(bazaar.getFromQueue(loc[0],loc[1]), Customer) or isinstance(bazaar.getFromQueue(loc[0],loc[1]), Staff)):
+                break
+        customer = Customer(random.choices(food_list,k=3),loc, bazaar)
+
+    time.sleep(1)
---
+++ q2.py
@@ -0,0 +1,198 @@
+import random
+from tkinter import *
+import threading
+import names
+import time
+from queue import Queue
+import numpy as np
+
+# This class is so that we can put threading.Lock to the queue
+class QueueClass(Queue) :
+    def __init__(self,maxSize,name) :
+        Queue.__init__(self,maxsize=maxSize)
+        self.Lock = threading.Lock() # for put queue function and increase self.Number
+        self.LockGet = threading.Lock() # for popping the queue
+        self.LockTime = threading.Lock() # for increasing time
+        self.Name = name # name of this queue
+        self.Time = 0 # waiting time in the queue
+        self.Number = 0 # number (for check in queue)
+
+simTime = 0
+simEnd = 30
+serverAmount = 3
+rejected = QueueClass(1000000,'Rejected') # for passenger who fail to board flight
+exit = QueueClass(1000000,'Exit') # for passenger who pass check in
+DoorQ = QueueClass(10000,'DOORQ')
+threads = [] # all threads
+
+OverallWaitingTime = 0 # total waiting time for all passengers
+OverallWaitingTimeLock = threading.Lock()
+CheckInServersQ = [] # list for check in queue
+CheckInServers = [] # list for check in server
+
+class CheckInServer(threading.Thread):
+    BUSY = 0
+    FREE = 1
+    def __init__(self,queue,ID):
+        threading.Thread.__init__(self)
+        self.Status = CheckInServer.FREE
+        self.Customer = None
+        self.Name = names.get_full_name()
+        self.Q = queue
+        self.ID = ID
+
+    def run(self):
+        global simTime, simEnd
+
+        while simTime < simEnd :
+            if self.Status == CheckInServer.FREE:
+                self.CheckGetCustomer()
+            if self.Status == CheckInServer.BUSY and self.Customer != None:
+                self.Status = CheckInServer.FREE
+
+                if not self.Customer.Passport:
+                    time.sleep(random.randint(0, 3))
+                    self.Customer.pos = Passenger.CHECKIN_REJECTED
+                    # add to rejected list
+                    rejected.Lock.acquire()
+                    rejected.put(self.Customer)
+                    rejected.Lock.release()
+                    # increasing the number served by the server
+                    self.Q.Lock.acquire()
+                    self.Q.Number += 1
+                    self.Q.Lock.release()
+                else:
+                    time.sleep(random.randint(0, 2))
+                    self.Customer.pos = Passenger.CHECKIN_DONE
+                    # add to done list
+                    exit.Lock.acquire()
+                    exit.put(self.Customer)
+                    exit.Lock.release()
+                    # increasing the number served by the server
+                    self.Q.Lock.acquire()
+                    self.Q.Number += 1
+                    self.Q.Lock.release()
+            time.sleep(1)
+
+    # function to get customer from queue
+    def CheckGetCustomer(self):
+        self.Q.LockGet.acquire()
+        if not self.Q.empty():
+            self.Customer = self.Q.get()
+            self.Status = CheckInServer.BUSY
+        else:
+            self.Customer = None
+        self.Q.LockGet.release()
+
+class Passenger(threading.Thread):
+    DOORQ_IN_QUEUE = 0
+    CHECKIN_IN_QUEUE = 1
+    CHECKIN_REJECTED = 3
+    CHECKIN_DONE = 4
+    
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.Ticket = np.random.choice([True,False],1,p=[0.98,1-0.98])
+        self.Passport = np.random.choice([True,False],1,p=[0.98,1-0.98])
+        self.Name = name
+        self.pos = Passenger.DOORQ_IN_QUEUE
+        self.Luggage = np.random.choice([True,False],1,p=[0.75,0.25])
+        self.Q = None
+        print(f'Passenger {self.Name} created.')
+    
+    def run(self):
+        global simTime, simEnd, CheckInServers, rejected, OverallWaitingTime, DoorQ
+
+        while simTime < simEnd:
+            # if passenger in queue, increate waiting time for queue and overall
+            if self.pos == Passenger.CHECKIN_IN_QUEUE:
+                OverallWaitingTimeLock.acquire()
+                OverallWaitingTime += 1
+                OverallWaitingTimeLock.release()
+                self.Q.LockTime.acquire()
+                self.Q.Time += 1
+                self.Q.LockTime.release()
+
+            if self.pos == Passenger.DOORQ_IN_QUEUE:
+                qsize = []
+                Break = False
+                for i in CheckInServersQ:
+                    if not i.full():
+                        if i.empty():
+                            self.Q = i
+                            self.Q.Lock.acquire()
+                            self.Q.put(self)
+                            self.Q.Lock.release()
+                            self.pos = Passenger.CHECKIN_IN_QUEUE
+                            Break = True
+                            print(f'{self.Name} went into {self.Q.Name}')
+                            break
+                        else:
+                            Break = False
+                            qsize.append(i.qsize())
+                if not Break:
+                    minVal = min(qsize)
+                    minIndex = qsize.index(minVal)
+                    self.Q = CheckInServersQ[minIndex]
+                    self.Q.Lock.acquire()
+                    self.Q.put(self)
+                    self.Q.Lock.release()
+                    self.pos = Passenger.CHECKIN_IN_QUEUE
+                    print(f'{self.Name} went into {self.Q.Name}')
+                
+            # if rejected                 
+            if  self.pos == Passenger.CHECKIN_REJECTED:
+                print(f'Passenger {self.Name} rejected')
+                break
+            # if done checkin  
+            if self.pos == Passenger.CHECKIN_DONE:
+                print(f'Passenger {self.Name} has done Check-in. Going to next procedure.')
+                break
+            time.sleep(1)
+
+
+# creating servers and server queue
+for i in range(serverAmount):
+    CheckInQ = QueueClass(10000,f'Q{i+1}')
+    # Assigning queue to server
+    CheckInServersQ.append(CheckInQ)
+    server = CheckInServer(CheckInServersQ[i],i)
+    CheckInServers.append(server)
+    CheckInServers[i].start()
+    threads.append(server)
+print('Servers Created.... Starting Simulation...')
+time.sleep(1)
+
+while simTime < simEnd:
+    if simTime+1 % 10 >=5: # if 5 minutes before flight time (peak hours)
+        NewPassengerAmount = random.randint(3,7)
+    else: # if not close to flight time (non peak hours)
+        NewPassengerAmount = random.randint(1,4)
+
+    for i in range(NewPassengerAmount):
+        
+        Name = names.get_full_name()
+        # create new passenger with the classes
+        newPass = Passenger(Name)
+        newPass.start() # start
+        DoorQ.put(newPass) # get the passenger into the list for initial queue
+        threads.append(newPass) # thread list to wait for all thread finish
+
+    time.sleep(1) # 1 minute delay
+    simTime += 1
+
+# thread list to wait for all thread finish
+for x in threads:
+    x.join()
+
+# output
+print('\nOUTPUT:')
+print(f'{DoorQ.qsize()} people went in.')
+for i in range(len(CheckInServersQ)):
+    print(f'Queue {i+1}')
+    print(f'    People still in queue: {CheckInServersQ[i].qsize()}')
+    print(f'    Served number: {CheckInServersQ[i].Number}')
+    print(f'    Overall Waiting Time for this queue: {CheckInServersQ[i].Time} Minutes.')
+print(f'Overall waiting time for all queue: {OverallWaitingTime} Minutes.')
+print(f'{rejected.qsize()} people has been rejected')
+print(f'{exit.qsize()} people have done check-in process and proceed to the next stage')
